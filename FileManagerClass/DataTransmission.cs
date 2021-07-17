16f89620fd4fe453abdd9f3a7e9a191e8e406bd1
Денис Скурат <den.skur2016@yandex.ru>
2021-07-17 21:43:35 +0300

    тест

---
+++ FileManagerClass/DataTransmission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FileManagerClass
+{
+    /// <summary>
+    /// Класс, отвечающий за передачу данных каким-либо путем
+    /// </summary>
+    public abstract class DataTransmission
+    {
+        public delegate void ProcessTransmittingHandler(object sender, double percent);
+        public event ProcessTransmittingHandler DataTransferStatus;
+        
+        public delegate void SendingStartHandler(object sender, bool status);
+        public event SendingStartHandler DataSendingStart;
+        
+        public delegate void SendingEndHandler(object sender, bool status);
+        public event SendingEndHandler DataSendingEnd;
+        
+        public abstract void Transmit(ITransmitThrowNetwork transmitThrowNetwork);
+        public abstract void Transmit(ITransmitInConsole transmitInConsole);
+    }
+}
